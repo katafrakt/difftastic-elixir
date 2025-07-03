0f2b4e2f96519d76372c8bcfbd658a8cfe772401
Paweł Świątkowski <katafrakt@vivaldi.net>
2025-07-03 17:39:08 +0200

    Prepare version 0.2.0

---
+++ Changelog.md
@@ -0,0 +1,3 @@
+## 0.2.0
+* Added `difft_assert_against_file` ExUnit assertion to make it possible to compare dynamic content with already existing file, without creating a temporary file for the latter.
+* Added basic snapshot testing capabilities via `Difftastic.SnapshotTest` module.
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Difftastic.MixProject do
     [
       app: :difftastic,
       description: "Syntax-aware diffs for Elixir, provided by Difftastic",
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
